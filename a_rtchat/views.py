5bb1093043f0063ad5866d9d419602e787867869
UMASANGAVI G <92853376+Umasangavi@users.noreply.github.com>
2024-05-08 09:49:46 +0530

    Add files via upload

---
+++ a_rtchat/views.py
@@ -0,0 +1,73 @@
+from django.shortcuts import render, get_object_or_404, redirect 
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.http import Http404
+from .models import *
+from .forms import *
+
+@login_required
+def chat_view(request, chatroom_name='public-chat'):
+    chat_group = get_object_or_404(Chat, chat_name= chatroom_name)
+    chat_messages = chat_group.chat_messages.all()[:30]
+    form = ChatmessageCreateForm()
+
+    other_user = None
+    if chat_group.is_private:
+        if request.user not in chat_group.members.all():
+            raise Http404()
+        for member in chat_group.members.all():
+            if member != request.user:
+                other_user = member
+                break
+
+    if request.htmx:
+        form = ChatmessageCreateForm(request.POST)
+        if form.is_valid:
+            message = form.save(commit=False)
+            message.author = request.user
+            message.group = chat_group
+            message.save()
+            context = {
+                'message' : message,
+                'user' : request.user
+            }
+            return render(request, 'a_rtchat/partials/chat_message_p.html', context)
+        
+    context = {
+        'chat_messages' : chat_messages, 
+        'form' : form,
+        'other_user' : other_user,
+        'chatroom_name' : chatroom_name,
+        'chat_group' : chat_group
+    }
+    
+    return render(request, 'a_rtchat/chat.html',context)
+
+@login_required
+def get_or_create_chatroom(request, username):
+    if request.user.username == username:
+        return redirect('home')
+    
+    other_user = User.objects.get(username=username)
+    my_chatrooms = request.user.chat_groups.filter(is_private=True)
+    
+    if my_chatrooms.exists():
+        for chatroom in my_chatrooms:
+            if other_user in chatroom.members.all():
+                chatroom = chatroom
+                break
+            else:
+                chatroom = Chat.objects.create(is_private=True)
+                chatroom.members.add(other_user, request.user)
+    else:
+        chatroom = Chat.objects.create(is_private=True)
+        chatroom.members.add(other_user, request.user)
+    print("Chatroom name:", chatroom.chat_name)
+    
+    if not chatroom.chat_name:
+ 
+        chatroom.chat_name = 'default-chatroom-name'
+        chatroom.save()
+        # return HttpResponse('Chatroom name is empty', status=400) # Example of returning an error response
+    
+    return redirect('chatroom', chatroom.chat_name)
